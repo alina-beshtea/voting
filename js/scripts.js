95ff3ed5339f90d9985ae07d2c7a711efffefc94
Dylan Manley and alina beshtea <dylan+alina@>
2019-10-24 11:02:46 -0700

    finish functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
 $(document).ready(function() {
-  $("form#ageCheck") = submit(function(event) {
-    var over18
+  var age = parseInt(prompt("how old are you?"));
 
-  if (age > 17) {
+  if (age >= 17) {
     $('#over18').show();
-    alert('please vote')
+
   } else {
     $('#under18').show();
-  alert('illigal')
+
 }
-});
 });
